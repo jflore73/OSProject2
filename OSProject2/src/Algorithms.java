7918c531b2995f9a226d08def70606ff38f20f3f
Jesus Flores Valdez <jflore73@students.kennesaw.edu>
2025-04-28 15:04:13 -0400

    Remaining two Algorithms implemented, must be revised

--- OSProject2/src/Algorithms.java
+++ OSProject2/src/Algorithms.java
@@ -8,12 +8,9 @@ public class Algorithms {
 		System.out.println("First Come First Served Algorithm");
 		System.out.print("Enter number of processes: ");
 		np=verify();
-		int np2=np*2;
 		double[] bp= new double[np];//Burst Time
 		double[] wtp = new double[np];//Wait Time
-		String[] output1 = new String[np2];
 		double twt = 0.0, awt; //Total Wait Time, Average Wait Time
-		int num;
 		for(int i=0; i<np;i++) {
 			System.out.print("Enter Burst time for Process "+i+": ");
 			bp[i]=verify();
@@ -39,7 +36,6 @@ public class Algorithms {
 		System.out.println("Shortest Job First Algorithm");
 		System.out.print("Enter number of processes: ");
 		np=verify();
-		int np2=np*2;
 		double[] bp= new double[np];//Burst Time
 		double[] wtp = new double[np];//Wait Time
 		double[] p= new double[np];//Process Order
@@ -95,6 +91,131 @@ public class Algorithms {
 		System.out.println("Average Wait Time: "+awt);
 	}
 	
+    public static void priorityAlgorithm(){
+    	int np;
+		System.out.println("Priority Schedule Algorithm");
+		System.out.print("Enter number of processes: ");
+		np=verify();
+		double[] bp = new double[np];
+		double[] wtp = new double[np + 1];
+		int[] p = new int[np];
+		int[] sp = new int[np];
+		int x, num;
+		double twt = 0.0;
+		double awt;
+		int temp = 0;
+		boolean found = false;
+		for(int i=0; i<np;i++) {
+			System.out.print("Enter Burst time for Process "+i+": ");
+			bp[i]=verify();
+		}
+		for(int i=0; i<np;i++) {
+			System.out.print("Enter Priority for Process "+i+": ");
+			p[i]=verify();
+		}
+		for(num = 0; num <= np - 1; num++){
+			sp[num] = p[num];
+		}
+		for(x = 0; x <= np - 2; x++){
+			for (num = 0; num <= np - 2; num++){
+				if (sp[num] > sp[num + 1]){
+					temp = sp[num];
+					sp[num] = sp[num + 1];
+					sp[num + 1] = temp;
+				}
+			}
+		}
+		for(num = 0; num <= np - 1; num++){
+			if (num == 0){
+				for (x = 0; x <= np - 1; x++){
+					if (sp[num] == p[x] && found == false){
+						wtp[num] = 0;
+						System.out.println("Waiting time for Process " + (x + 1) + " = " + wtp[num]);
+						temp = x;
+						p[x] = 0;
+						found = true;
+					}
+				}
+				found = false;
+			}
+			else{
+				for (x = 0; x <= np - 1; x++){
+					if (sp[num] == p[x] && found == false){
+						wtp[num] = wtp[num - 1] + bp[temp];
+						System.out.println("Waiting time for Process " + (x + 1) + " = " + wtp[num]);
+						temp = x;
+						p[x] = 0;
+						found = true;
+					}
+				}
+				found = false;
+			}
+		}
+		for(int i=0; i<np;i++){
+			twt+=(double)wtp[i];
+		}
+		System.out.println("Total Wait Time: "+twt);
+		awt=twt/(double)np;
+		System.out.println("Average Wait Time: "+awt);
+	}
+    
+    public static void roundRobinAlgorithm(){
+    	int np;
+		System.out.println("Round Robin Algorithm");
+		System.out.print("Enter number of processes: ");
+		np=verify();
+		int i, counter = 0;
+		double total = 0.0;
+		double timeQuantum;
+		double waitTime = 0, turnaroundTime = 0;
+		double averageWaitTime, averageTurnaroundTime;
+		double[] arrivalTime = new double[10];
+		double[] burstTime = new double[10];
+		double[] temp = new double[10];
+		int x = np;
+		for (i = 0; i < np; i++){
+			System.out.print("Enter Arrival time for Process "+(i+1)+": ");
+			arrivalTime[i] = verify();
+			System.out.print("Enter Burst time for Process "+(i+1)+": ");
+			burstTime[i] = verify();
+			temp[i] = burstTime[i];
+		}
+		System.out.print("Enter Time Quantum: ");
+		timeQuantum= verify();
+		for (total = 0, i = 0; x != 0;){
+			if (temp[i] <= timeQuantum && temp[i] > 0){
+				total = total + temp[i];
+				temp[i] = 0;
+				counter = 1;
+			}
+			else if (temp[i] > 0){
+				temp[i] = temp[i] - timeQuantum;
+				total = total + timeQuantum;
+			}
+			if (temp[i] == 0 && counter == 1){
+				x--;
+				System.out.println("Turnaround time for Process " + (i + 1) + " : " + (total - arrivalTime[i]));
+				System.out.println("Wait time for Process " + (i + 1) + " : " + (total - arrivalTime[i] - burstTime[i]));
+				turnaroundTime = (turnaroundTime + total - arrivalTime[i]);
+				waitTime = (waitTime + total - arrivalTime[i] - burstTime[i]);
+				counter = 0;
+			}
+			if (i == np - 1){
+				i = 0;
+			}
+			else if (arrivalTime[i + 1] <= total){
+				i++;
+			}
+			else{
+				i = 0;
+			}
+		}
+		averageWaitTime = (int)(waitTime * 1.0 / np);
+		averageTurnaroundTime = (int)(turnaroundTime * 1.0 / np);
+		System.out.println("Average wait time for " + np + " processes: " + averageWaitTime + " sec(s)");
+		System.out.println("Average turnaround time for " + np + " processes: " + averageTurnaroundTime + " sec(s)");
+	}
+    
 	private static int verify(){
 		int input=-10;
 		boolean isValid=false;
--- OSProject2/src/ProjectDriver.java
+++ OSProject2/src/ProjectDriver.java
@@ -1,17 +1,66 @@
 import java.util.*;
 public class ProjectDriver {
 	private static Scanner keyboard=new Scanner(System.in);
-
+	
 	public static void main(String[] args) {
 		int input;
-		System.out.print("Enter an integer: ");
-		input=verify();
-		System.out.println(input);
-		Algorithms.fcfsAlgorithm();
-		System.out.println();
-		Algorithms.sjfAlgorithm();
+		do {
+			input=display();
+			switch(input) {
+			case 1: 
+				Algorithms.fcfsAlgorithm();
+				break;
+			case 2: 
+				Algorithms.sjfAlgorithm();
+				break;
+			case 3:
+				Algorithms.priorityAlgorithm();
+				break;
+			case 4:
+				Algorithms.roundRobinAlgorithm();
+				break;
+			case 5:
+				System.out.println();
+				break;
+			case 6:
+				System.out.println();
+				break;
+			case 7:
+				System.out.println();
+				break;
+			case 8:
+				System.out.println();
+				break;
+			case 9:
+				System.out.println();
+				break;
+			case 10:
+				break;
+			default:
+				System.out.println("Invalid input. Try again.");
+				break;
+			}
+			System.out.println();
+		}while(input!=10);
 
 	}
+	private static int display() {
+		System.out.println("------------------MAIN MENU------------------");
+		System.out.println("1. First Come First Serve Algorithm");
+		System.out.println("2. Shortest Job First Algorithm");
+		System.out.println("3. Priority Schedule Algorithm");
+		System.out.println("4. Round Robin Algorithm");
+		System.out.println("5. Undefined");
+		System.out.println("6. Undefined");
+		System.out.println("7. Undefined");
+		System.out.println("8. Undefined");
+		System.out.println("9. Undefined");
+		System.out.println("10. Exit Program");
+		System.out.println();
+		System.out.print("Enter option number: ");
+		return verify();
+	}
+	
 	private static int verify(){
 		int number=-1;
 		boolean isValid = false;
